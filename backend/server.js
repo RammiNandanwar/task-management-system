bb9733b8bc523367d7679c692f4886afa49dd5ec
Rammi _Nandanwar0425 <ramminandanwar@gmail.com>
2026-08-13 20:49:06 +0530

    Add authentication routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authroute");
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
