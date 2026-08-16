96972327e1b05caef112538177836f33ac9d6698
Rammi _Nandanwar0425 <ramminandanwar@gmail.com>
2026-08-16 21:51:09 +0530

    Add JWT token to API requests

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,4 +7,20 @@ const API = axios.create({
     }
 });
 
+// Add JWT token to every request
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 export default API;
