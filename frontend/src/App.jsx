682ba51d4c2451af43ef0793f911fb8857aa1063
Rammi _Nandanwar0425 <ramminandanwar@gmail.com>
2026-08-16 21:48:31 +0530

    Add protected dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+
+                {/* Default page */}
+                <Route path="/" element={<Navigate to="/login" replace />} />
+
+                {/* Public routes */}
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+
+                {/* Protected routes */}
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+
             </Routes>
         </BrowserRouter>
     );
